0ef283cc4a89bf4bd238518089a254df73dbd09b
Daksha Patel <dakshapatel100@gmail.com>
2018-05-02 18:41:28 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array= ["honda", "toyota", "nissan", "volvo"]
+  array << "subaru"
+  puts array.inspect 
 end
 
 def add_element_to_start_of_array(array, element)
